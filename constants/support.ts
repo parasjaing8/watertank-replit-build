3366c9a9716586555bfc2d12deb97ec5fdd9a1f9
parasjang7 <parasgidd@gmail.com>
2026-05-26 22:35:12 +0530

    chore: set SUPPORT_WHATSAPP_NUMBER for Report a Problem feature

--- constants/support.ts
+++ constants/support.ts
@@ -1,3 +1,3 @@
 // Developer WhatsApp number for customer issue reports.
 // Format: country code + number, no + or spaces. e.g. "919876543210"
-export const SUPPORT_WHATSAPP_NUMBER = "91XXXXXXXXXX"; // TODO: replace with your actual number
+export const SUPPORT_WHATSAPP_NUMBER = "918237779153"; // TODO: replace with your actual number
